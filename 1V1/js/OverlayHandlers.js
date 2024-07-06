720aadcc7ae0954c1773e0e91a2c6ed91902f83b
Kasper <kasperjnssen@gmail.com>
2024-07-06 21:03:10 +0200

    corrected format and overlay handlers

--- 1V1/js/OverlayHandlers.js
+++ 1V1/js/OverlayHandlers.js
@@ -1,13 +1,13 @@
 async function setOverlay(playerIDs, playerNames, Round) {
 	document.getElementById("TextBox").style.opacity = "0";
-	fetch('https://spi.danesaber.cf/api/bs/ss/' + playerIDs[0])
+	fetch('https://skillsaber.vercel.app/api/player?id=' + playerIDs[0])
 		.then(response => response.json())
 		.then(data => {
 			document.getElementById("Player1Image").src = data.profilePicture;
 			document.getElementById("Player1Name").innerText = playerNames[0];
 			document.getElementById("Player1Name").style.opacity = '1';
 		});
-	fetch('https://spi.danesaber.cf/api/bs/ss/' + playerIDs[1])
+	fetch('https://skillsaber.vercel.app/api/player?id=' + playerIDs[1])
 		.then(response => response.json())
 		.then(data => {
 			document.getElementById("Player2Image").src = data.profilePicture;
